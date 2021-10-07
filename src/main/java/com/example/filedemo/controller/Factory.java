8e398747a519ab104f90b2e12a0d1d446e926728
JoDeMiro <joedemiro@pintye.com>
2021-10-07 11:18:15 +0200

    some changes must be deleted

---
+++ src/main/java/com/example/filedemo/controller/Factory.java
@@ -0,0 +1,29 @@
+package com.example.filedemo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.*;
+
+@Component
+public class Factory {
+
+    @Bean
+    public void run() {
+
+        int corePoolSize = 2;
+        int maximumPoolSize = 5;
+        int keepAliveTime = 1000;
+        int queueSize = 3;
+
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAliveTime, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(queueSize), threadFactory, new RejectionHandler());
+
+        for (int i = 0; i < 10; i++) {
+            poolExecutor.execute(new Job(i));
+        }
+
+        poolExecutor.shutdown();
+    }
+}
