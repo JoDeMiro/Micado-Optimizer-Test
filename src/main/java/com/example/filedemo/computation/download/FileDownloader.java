a359dcb7dfa030e203a0faef5099bc1b87f2c959
JoDeMiro <joedemiro@pintye.com>
2021-10-10 17:39:11 +0200

    new download from spring JMeter test

---
+++ src/main/java/com/example/filedemo/computation/download/FileDownloader.java
@@ -0,0 +1,12 @@
+package com.example.filedemo.computation.download;
+
+public class FileDownloader {
+
+
+
+
+
+
+
+    // https://github.com/JoDeMiro/Data/raw/main/Large/2048_2048.png
+}
---
+++ src/main/java/com/example/filedemo/computation/download/RestDownloader.java
@@ -0,0 +1,7 @@
+package com.example.filedemo.computation.download;
+
+public class RestDownloader {
+
+
+    // https://github.com/JoDeMiro/Data/raw/main/Large/2048_2048.png
+}
