76eddcf7b9db5b2d13bcf91c764ec359bcf180ca
JoDeMiro <joedemiro@pintye.com>
2024-03-22 12:36:03 +0100

    Update: loadbalancer.conf -> /cpu/

--- src/main/java/com/example/filedemo/FileDemoApplication.java
+++ src/main/java/com/example/filedemo/FileDemoApplication.java
@@ -4,7 +4,8 @@ import com.example.filedemo.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /** Main class
  * @author JoDeMiro
@@ -22,4 +23,10 @@ public class FileDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileDemoApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
--- src/main/java/com/example/filedemo/controller/RestsControllerWithTrace.java
+++ src/main/java/com/example/filedemo/controller/RestsControllerWithTrace.java
@@ -1,16 +1,19 @@
 package com.example.filedemo.controller;
 
 import brave.Span;
+import brave.SpanCustomizer;
 import brave.Tracer;
 import com.example.filedemo.service.SleuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -27,10 +30,6 @@ public class RestsControllerWithTrace {
     @Autowired
     private Tracer tracer;
 
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
 
     @RequestMapping("/A")
     public String A() throws InterruptedException {
@@ -43,24 +42,20 @@ public class RestsControllerWithTrace {
     public String B() throws InterruptedException {
         logger.info("you called B");
         Thread.sleep(500);
-        RestTemplate restTemplate = getRestTemplate();
-        return restTemplate.getForObject("http://localhost/A", String.class);
+        return restTemplate.getForObject("http://localhost:8080/A", String.class);
     }
 
     @RequestMapping("/C")
     public String C() throws InterruptedException {
         logger.info("you called C");
         Thread.sleep(1000);
-        RestTemplate restTemplate = getRestTemplate();
-        return restTemplate.getForObject("http://localhost/B", String.class);
+        return restTemplate.getForObject("http://localhost:8080/B", String.class);
     }
-
 
     @RequestMapping("/RB")
     public String RB() throws InterruptedException {
         logger.info("you called B");
         Thread.sleep(500);
-        RestTemplate restTemplate = getRestTemplate();
         return restTemplate.getForObject("http://localhost/A", String.class);
     }
 
@@ -68,54 +63,12 @@ public class RestsControllerWithTrace {
     public String RC() throws InterruptedException {
         logger.info("you called RC");
         Thread.sleep(1000);
-        RestTemplate restTemplate = getRestTemplate();
         return restTemplate.getForObject("http://" + remoteServiceAccessPoint + "/RB", String.class);
     }
 
-    @RequestMapping("/RPrime")
-    public String RPrime() throws InterruptedException {
-        logger.info("you called RPrime");
-        Thread.sleep(1000);
-        RestTemplate restTemplate = getRestTemplate();
-        String url = "http://" + remoteServiceAccessPoint + "/cpu/prime/10";
-        System.out.println(url);
-        return restTemplate.getForObject(url, String.class);
-    }
 
-    @GetMapping("/T1")
-    public String t1() {
-        logger.info("Hello with Sleuth T1");
-        Span span = tracer.currentSpan();
-        if (span != null) {
-            logger.info("Span ID decimal {}", span.context().spanId());
-            logger.info("Trace ID decimal {}", span.context().traceId());
-        }
-        return "Hello T1";
-    }
 
-    @GetMapping("/T2")
-    public String t2() {
-        logger.info("Hello with Sleuth T2");
-        RestTemplate restTemplate = getRestTemplate();
-        Span span = tracer.currentSpan();
-        if (span != null) {
-            logger.info("Span ID decimal {}", span.context().spanId());
-            logger.info("Trace ID decimal {}", span.context().traceId());
-        }
-        return restTemplate.getForObject("http://localhost:8080/T1", String.class);
-    }
 
-    @GetMapping("/T21")
-    public String t21() {
-        logger.info("Hello with Sleuth T2");
-        RestTemplate restTemplate = getRestTemplate();
-        Span span = tracer.currentSpan();
-        if (span != null) {
-            logger.info("Span ID decimal {}", span.context().spanId());
-            logger.info("Trace ID decimal {}", span.context().traceId());
-        }
-        return restTemplate.getForObject("http://localhost:8080/T2", String.class);
-    }
 
     @Autowired
     private SleuthService sleuthService;
@@ -134,10 +87,124 @@ public class RestsControllerWithTrace {
         return "success";
     }
 
+
+
+
+
+
+
+
+
+
+
+    @RequestMapping("/RPrime")
+    public String RPrime() throws InterruptedException {
+        logger.info("you called RPrime");
+        Thread.sleep(1000);
+
+        String url = "http://" + remoteServiceAccessPoint + "/cpu/prime/10";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @RequestMapping(value = "/RP/{interest}")
+    public String D(@PathVariable String interest) throws InterruptedException {
+        logger.info("you called RP");
+        Thread.sleep(1000);
+
+        String url = "http://" + remoteServiceAccessPoint + "/cpu/prime/10";
+
+        String responseNewType = restTemplate.exchange(url,
+                HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, interest).getBody();
+
+        logger.info("Return value of responseNewType");
+        System.out.println(responseNewType);
+        logger.info("-------------------------------");
+
+        String responseOldType = restTemplate.getForObject(url, String.class);
+
+        return responseOldType;
+    }
+
+    @RequestMapping("/RD")
+    public String RD() throws InterruptedException {
+        logger.info("you called RD");
+        Thread.sleep(500);
+
+        // Ez a megoldás nem jó
+        // RestTemplate restTemplate0 = getRestTemplate();
+        // return restTemplate0.getForObject("http://localhost:8082/A", String.class);
+
+        // Ez amit Bean-ben van és ha jól értem akkor Autowire-d és jó megoldás
+        return restTemplate.getForObject("http://localhost:8082/A", String.class);
+    }
+
+    // Most éppen ezt vizsgálgatok, hogy kell-e külön a ResponseEntytit megcsálnom
+    // vagy működik-e anélkül is
+    // Ha így csinálom meg akkor megjelenik ugyan a zipkinbe, de nincs összekötve
+    // Hanem külön trace ID-t kap hiába hív át egy másik APP-bva egy másik servicebe
+    @GetMapping("/ZA")
+    public String za() {
+        logger.info("Hello with Sleuth ZA");
+        Span span = tracer.currentSpan();
+        if (span != null) {
+            logger.info("Span ID decimal {}", span.context().spanId());
+            logger.info("Trace ID decimal {}", span.context().traceId());
+        }
+        return restTemplate.getForObject("http://localhost:8082/ZB", String.class);
+    }
+
+    @GetMapping("/ZB")
+    public String zb() {
+        logger.info("Hello with Sleuth ZB");
+        Span span = tracer.currentSpan();
+
+        // Extra information attached to the span
+        SpanCustomizer customizer = span.customizer();
+        customizer.tag("egyke", "kettőke");
+
+        if (span != null) {
+            logger.info("Span ID decimal {}", span.context().spanId());
+            logger.info("Trace ID decimal {}", span.context().traceId());
+        }
+        return "Hello ZB";
+    }
+
+
+
+    // Ez is jó megoldás, az összes a Bean restTemplate objektumot használja ez nem
+
+    // Ehhez egyébként kétszer kell elindítani a Springet.
+    // Az egyik a 8080 porton fut
+    // A másik viszont a 8082 proton fut
+    // ava -Xms1024m -Xmx2048m -jar file-demo-0.0.1-SNAPSHOT.jar --server.port=8082 --spring.application.name=ServiceB
+
+    @GetMapping("/KA")
+    public String ka() {
+        logger.info("Hello KA");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "farok");
+
+        HttpEntity request = new HttpEntity(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                "http://localhost:8082/KB",
+                HttpMethod.GET,
+                request,
+                String.class
+        );
+
+        String result = response.getBody();
+        logger.info("Reply = " + result);
+
+        return "Hello from /a - " + "Faszom" + ", " + result;
+    }
+
+    @GetMapping("/KB")
+    String kb(@RequestHeader(name = "Authorization") String authToken) {
+        logger.info("Handling b - " + "appName");
+        return "Hello from /b - " + "appName";
+    }
+
 }
-
-
-
-
-
-
